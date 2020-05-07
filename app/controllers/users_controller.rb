478a915b1c8ce2831ad628571c866feb53927d4c
Tushar Desai <tushar.sunil.desai@gmail.com>
2020-05-07 04:26:14 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params) #Not the final implementation
+    #U
+    if @user.save
+      redirect_to @user
+      flash[:success] = "Welcome to the Sample app"
+      #handle a successful save
+    else
+      #Renders the new template now with a @user object that has many errors
+      #associated with it
+      render 'new'
+    end
+  end
+  
+  private
+
+    def user_params
+      return params.require(:user).permit(:name, :email, :password,:password_confirmation)
+    end
 end
+  
+
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+  
+  
+  # Returns the Gravatar for the given user
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    #Technically, since views are just made up with ruby methods <%=..%>
+    #we can just have this method return an image_tag method and then insert
+    #this into the view
+    
+    #Notice how you can specific a class in this image tag, that's sick bro
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
